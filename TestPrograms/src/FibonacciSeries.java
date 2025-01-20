9d9cb111cbd9621524a0647fd2ed44070b52da56
Guruprasad <guru8080.gp@gmail.com>
2025-01-20 21:52:46 +0530

    Patch 7
    Uploaded

---
+++ TestPrograms/src/FibonacciSeries.java
@@ -0,0 +1,21 @@
+public class FibonacciSeries {
+    public static void main(String[] args){
+        int n=10;
+        System.out.println("Fibboncci Series: "+fibonacci(n));
+    }
+
+    public static int fibonacci(int n){
+        if(n<=1){
+            return n;
+        }
+        int prev1=1,prev2=0;
+        int current=0;
+        for(int i=2;i<n;i++){
+          //  System.out.println(""+prev2); line is to print the fibo series
+            current=prev2+prev1;
+            prev2=prev1;
+            prev1=current;
+        }
+        return current;
+    }
+}
--- TestPrograms/src/LinkedListPalindrome.java
+++ TestPrograms/src/LinkedListPalindrome.java
@@ -58,8 +58,5 @@ class ListNode{
     ListNode(int val){
         this.val=val;
     }
-    ListNode(int val ,ListNode next){
-        this.val=val;
-        this.next=next;
-    }
+
 }
